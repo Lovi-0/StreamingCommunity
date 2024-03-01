fbca52d89f767fc43af3d4b04872f7e748b7b4ba
KastroMugnaio <65294975+KastroMugnaio@users.noreply.github.com>
2024-03-01 16:08:01 +0100

    Update page.py (#47)

--- Src/Api/page.py
+++ Src/Api/page.py
@@ -8,45 +8,39 @@ from Src.Util.console import console
 import requests, sys, json
 from bs4 import BeautifulSoup
 
+
 def domain_version():
+    domain_req = requests.get("https://api.telegra.ph/getPage/Link-Aggiornato-StreamingCommunity-01-17")
+    domain = domain_req.json()['result']['description']
     console.print("[green]Get rules ...")
     req_repo = None
-    try:
-        with open('data.json', 'r') as file:
-            req_repo = json.load(file)
-    except FileNotFoundError:
-        req_repo = {"domain": ""}
-    domain = req_repo['domain']
 
     while True:
-        if not domain:
-            domain = input("Insert domain (streamingcommunity.?): ")
-            req_repo['domain'] = domain
-            with open('data.json', 'w') as file:
-                json.dump(req_repo, file)
         console.print(f"[blue]Test domain [white]=> [red]{domain}")
-        site_url = f"https://streamingcommunity.{domain}"
+        site_url = domain
         try:
             site_request = requests.get(site_url, headers={'user-agent': get_headers()})
             soup = BeautifulSoup(site_request.text, "lxml")
             version = json.loads(soup.find("div", {"id": "app"}).get("data-page"))['version']
             console.print(f"[blue]Rules [white]=> [red].{domain}")
             return domain, version
-        
+
         except Exception as e:
             console.log("[red]Cant get version, problem with domain. Try again.")
             domain = None
             continue
 
-def search(title_search, domain):
 
-    req = requests.get(f"https://streamingcommunity.{domain}/api/search?q={title_search}", headers={'user-agent': get_headers()})
+def search(title_search, domain):
+    req = requests.get(f"{domain}/api/search?q={title_search}", headers={'user-agent': get_headers()})
 
     if req.ok:
-        return [{'name': title['name'], 'type': title['type'], 'id': title['id'], 'slug': title['slug']} for title in req.json()['data']][0:21]
+        return [{'name': title['name'], 'type': title['type'], 'id': title['id'], 'slug': title['slug']} for title in
+                req.json()['data']][0:21]
     else:
         console.log(f"[red]Error: {req.status_code}")
         sys.exit(0)
+
 
 def display_search_results(db_title):
     for i, title in enumerate(db_title):
