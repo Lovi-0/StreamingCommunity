c71801e4a0c75a7369a01f5f00b519d6cbae201c
Francesco Grazioso <francescograzioso1995@gmail.com>
2024-04-18 14:41:52 +0200

    start implementing download endpoint (only movie for now)

--- api/endpoints/urls.py
+++ api/endpoints/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 
-from .views import SearchView
+from .views import SearchView, DownloadView
 
 router = routers.DefaultRouter()
 
 router.register(r"search", SearchView, basename="search")
+router.register(r"download", DownloadView, basename="download")
 
 urlpatterns = router.urls
