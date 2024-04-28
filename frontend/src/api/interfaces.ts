ad3d8b61924252d3fee2a815b8da3f50a7a3bce4
Francesco Grazioso <francescograzioso1995@gmail.com>
2024-04-28 18:48:57 +0200

    start

---
+++ frontend/src/api/interfaces.ts
@@ -0,0 +1,24 @@
+interface Image {
+    imageable_id: number;
+    imageable_type: string;
+    filename: string;
+    type: string;
+    original_url_field: string;
+}
+
+export interface MediaItem {
+    id: number;
+    slug: string;
+    name: string;
+    type: string;
+    score: string;
+    sub_ita: number;
+    last_air_date: string;
+    seasons_count: number;
+    images: Image[];
+    comment: string;
+}
+
+export interface MediaItemResponse {
+    media: MediaItem[];
+}
