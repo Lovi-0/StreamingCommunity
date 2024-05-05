0405ca7d21cc2ebde0d505f6cc46bb9d4b7f5525
Francesco Grazioso <francescograzioso1995@gmail.com>
2024-05-05 12:59:09 +0200

    add download for all anime episodes

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -30,15 +30,17 @@ export async function getEpisodesInfo(mediaId: number, mediaSlug: string, mediaT
   });
 }
 
-async function downloadMedia(mediaId: number, mediaSlug: string, mediaType: string): Promise<AxiosResponse<DownloadResponse>> {
+async function downloadMedia(mediaId: number, mediaSlug: string, mediaType: string, downloadId?: number): Promise<AxiosResponse<DownloadResponse>> {
   const url = `/download/`;
   const data = {
     media_id: mediaId,
     media_slug: mediaSlug,
     type_media: mediaType,
+    download_id: downloadId,
   };
   return post(url, data);
 }
 
 export const downloadFilm = (mediaId: number, mediaSlug: string) => downloadMedia(mediaId, mediaSlug, 'MOVIE');
 export const downloadAnimeFilm = (mediaId: number, mediaSlug: string) => downloadMedia(mediaId, mediaSlug, 'OVA');
+export const downloadAnimeSeries = (mediaId: number, mediaSlug: string, downloadId: number) => downloadMedia(mediaId, mediaSlug, 'OVA', downloadId);
