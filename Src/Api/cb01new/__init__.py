bdbcfb2fd1b3c1c865ce501f7232321dc3abf1e3
Lovi <62809003+Lovi-0@users.noreply.github.com>
2024-11-14 08:30:53 +0100

    Re add cb01

---
+++ Src/Api/cb01new/__init__.py
@@ -0,0 +1,42 @@
+# 09.06.24
+
+# Internal utilities
+from Src.Util.console import console, msg
+
+
+# Logic class
+from .site import title_search, run_get_select_title
+from .film import download_film
+
+
+# Variable
+indice = 9
+_useFor = "film"
+_deprecate = False
+_priority = 2
+_engineDownload = "mp4"
+
+
+def search():
+    """
+    Main function of the application for film and series.
+    """
+
+    # Make request to site to get content that corrsisponde to that string
+    string_to_search = msg.ask("\n[purple]Insert word to search in all site").strip()
+    len_database = title_search(string_to_search)
+
+    if len_database > 0:
+
+        # Select title from list
+        select_title = run_get_select_title()
+
+        # !!! ADD TYPE DONT WORK FOR SERIE
+        download_film(select_title)
+
+
+    else:
+        console.print(f"\n[red]Nothing matching was found for[white]: [purple]{string_to_search}")
+
+        # Retry
+        search()
