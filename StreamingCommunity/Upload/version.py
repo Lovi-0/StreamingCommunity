a3f17e0e71f5cbcabd43b692af0d11a66873741e
Lovi <62809003+Lovi-0@users.noreply.github.com>
2024-12-23 16:59:26 +0100

    v1.9.8

--- StreamingCommunity/Upload/version.py
+++ StreamingCommunity/Upload/version.py
@@ -1,5 +1,5 @@
 __title__ = 'StreamingCommunity'
-__version__ = '1.9.6'
+__version__ = '1.9.8'
 __author__ = 'Lovi-0'
 __description__ = 'A command-line program to download film'
 __copyright__ = 'Copyright 2024'
